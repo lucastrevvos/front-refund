aff59d9443bfd2ce61b281579072a9b12ae71dc7
Lucas Amaral <lucas.trevvos@gmail.com>
2025-04-26 23:25:12 -0300

    api integration user post

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,24 @@
 import { useState } from "react";
+import { z, ZodError } from "zod";
+import { useNavigate } from "react-router";
+import { api } from "../services/api";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { AxiosError } from "axios";
+
+const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "E-mail invãlido" }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve ter pelo menos 6 dígitos" }),
+    passwordConfirm: z.string({ message: "Confirme a senha" }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais",
+    path: ["passwordConfirm"],
+  });
 
 export function SignUp() {
   const [name, setName] = useState("");
@@ -9,10 +27,40 @@ export function SignUp() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    console.log(email, password);
+    try {
+      setIsLoading(true);
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+
+      await api.post("/users", data);
+
+      if (confirm("Cadastrado com sucesso. Ir para tela de entrar ? ")) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não foi possivel cadastrar!");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
